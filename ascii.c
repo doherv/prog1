e0815ed3f77b544ca0d2597b2e4aa4e36ed745b8
vgdoherty <vgdoherty@gmail.com>
2016-11-09 18:37:21 -0500

    hey

--- ascii.c
+++ ascii.c
@@ -10,8 +10,12 @@ int main()
   printf("\nEntre com uma letra:\n");
   scanf("%c", &letra);
   printf("caractere = %c\ncodigo = %d\n", letra, letra);
-
-  if( (letra>64) && (letra<91) )  
+  
+  if( (letra>47) && (letra<58) )
+  {
+    printf("Voce digitou um numero!\n");
+  }
+  else if( (letra>64) && (letra<91) )  
   {
     printf("A letra e maiuscula!\n");
   }
